31414ddf2d90a2ed938c1ae845b93f9fed49fa3d
Vincent Agnano <vincent.agnano@scopyleft.fr>
2016-07-22 23:29:39 +0200

    initial publication with demo

---
+++ README.md
@@ -0,0 +1,45 @@
+# SPAger
+
+SPAger is a SPA (Single Page Application) multi paging module based on routing.
+It is meant to be very light (<2Kb), modular, plug-and-play yet basic.
+
+It is a standalone [Vanilla](http://vanilla-js.com/) JavaScript lib.
+
+It is based on native browser anchors.
+
+
+## Example
+
+```
+<section class=page>
+  <h1>Welcome home!</h1>
+  <a href="#contact">See contact infos</a>
+</section>
+<section class=page id=contact>
+  <h1>Contact me<h1>
+  <a href="#">Back home</a>
+</section>
+
+<script src="js/spager.js"></script>
+<script>spager.init()</script>
+```
+
+Here we have 2 pages: _home_ and _contact_.
+Pages are detected by the `class=page` attribute and the page name by the _id_ attribute.
+The page change is executed based the hash in the URL.
+Easy and logic!
+
+`spager.init()` will initialize the spager.
+
+
+## API
+
+`spager.init()`: initializes the pager. It consists of hiding all pages except
+the homepage.
+
+`spager.home`: string that defines the id of the homepage. blank by default.
+
+`spager.error404`: string that defines the page to display when the hash was
+not found.
+
+`spager.goto(pageName)`: changes the page to go to _pageName_ (string).
---
+++ src/spager.js
@@ -0,0 +1,94 @@
+(function(module) {
+  var pager = {
+    // DOM NodeList (or any iterable) of the pages.
+    pages: document.querySelectorAll('.page'),
+    // id of the page to display by default.
+    home: '',
+    // id attribute of the page to display when the one requested was not found.
+    error404: '',
+
+    /**
+    * Bootstrap the app:
+    *   - hide all pages
+    *   - listen for page changes
+    *   - display home page.
+    */
+    init: function() {
+      var self = this
+      window.addEventListener('hashchange', function(event) {
+        self.show(self.getCurrentHash())
+      })
+
+      this.hideAll()
+      this.show(this.getCurrentHash())
+    },
+
+    /**
+    * Retrieves the current hash.
+    * @return String
+    */
+    getCurrentHash: function() {
+      return document.location.hash.slice(1)
+    },
+
+    /**
+    * Checks that the current pages matches a page name, or home.
+    * @return Boolean
+    */
+    isCurrent: function(name) {
+      if(name == this.getCurrentHash()) {
+        return true
+      }
+      if(!name && this.getCurrentHash() == this.home) {
+        return true
+      }
+      return false
+    },
+
+    /**
+    * Hide all pages
+    */
+    hideAll: function() {
+      [].forEach.call(this.pages, function(page) {
+        page.style.display = 'none'
+      })
+    },
+
+    /**
+    * Find the Node that matches the home best.
+    * @return DOMNode
+    */
+    getHome: function() {
+      return this.home ? document.getElementById(this.home)
+                       : this.pages[0]
+    },
+
+    /**
+    * Modifies the pages DOM Nodes to display only the page matching 'name'.
+    */
+    show: function(name) {
+      this.hideAll()
+
+      var target
+      if(name) {
+        target = document.getElementById(name)
+      }
+      else {
+        target = this.getHome()
+      }
+      if(!target) {
+        target = document.getElementById(this.error404) || this.getHome()
+      }
+      target.style.display = 'inherit'
+    },
+
+    /**
+    * Requests a page change call.
+    */
+    goto: function(name) {
+      document.location.hash = name || ' '
+    }
+  }
+
+  module.spager = pager
+})(this)
